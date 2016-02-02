33f11bc36caf3cca9bbe06d3980811eac5124dac
SN4T14 <SN4T14@users.noreply.github.com>
2016-02-02 01:57:16 +0000

    Removed unnecessary crap

--- main.js
+++ main.js
@@ -7,18 +7,6 @@ var Promise = require('bluebird');
 var config = require('./lib/loadConfig')('config.yml');
 var database = require('./lib/dal/' + config.database)(config[config.database]);
 
-var plugins = [];
-
 Object.keys(config.plugins).forEach(function (pluginName) {
-	plugins.push(require('./plugins/' + pluginName)(config.pluginGlobals, config.plugins[pluginName], database));
+	require('./plugins/' + pluginName)(config.pluginGlobals, config.plugins[pluginName], database);
 });
-
-/*Promise.try(function() {
-	return database.writePoint("testSeries", 6, {tagA: "asdf", tagB: "qwerty"});
-}).then(function (response) {
-	console.log("Success!");
-	console.log(response);
-}).catch(function (err) {
-	console.log("Error!");
-	console.log(err);
-});*/
